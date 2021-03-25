838d6146e9f01e361dc60c7c5266fbb7c0316bf3
Kelsey McCutcheon <kelsey.mccutcheon@mymail.champlain.edu>
2021-03-25 02:11:31 -0400

    something working for backend, its not nice looking but...

--- app.py
+++ app.py
@@ -11,21 +11,71 @@ def number_sense_test():
     data = pd.read_csv("NumberSenseQuestions.csv")
     ques_and_ans = []
     for question in data.QuestionD1:
+        '''
         ques_dict = {
             "question": question,
-            "answer": answer
+            "answer": answer(question)
         }
+        print(ques_dict)
         ques_and_ans.append(ques_dict)
+        '''
+        ques_and_ans.append(question)
+        user_answer = request.args.get('user_answer')
     return ques_and_ans
 
 
-def answer(question):
-    correct = eval(question)
+def answer(test_ans):
+    return test_ans
+
+
+'''
+def answer(test_ans):
+    print(test_ans)
+    correct = eval(test_ans)
     user_answer = request.args.get('user_answer')
     if user_answer == correct:
         return "Correct!"
     else:
         return "Incorrect!"
+'''
+
+
+# allow both GET and POST requests
+@app.route('/test', methods=['GET', 'POST'])
+def test():
+    data = pd.read_csv("NumberSenseQuestions.csv")
+    question1 = data.QuestionD1[1]
+    question2 = data.QuestionD1[2]
+    question3 = data.QuestionD1[3]
+    question4 = data.QuestionD1[4]
+    question5 = data.QuestionD1[5]
+    """
+    answer1 = request.form.get('answer1')
+    answer2 = request.form.get('answer2')
+    answer3 = request.form.get('answer3')
+    answer4 = request.form.get('answer3')
+    answer5 = request.form.get('answer3')"""
+
+    # handle the POST request
+    if request.method == 'POST':
+        return '''
+                  <h2>Correct Answer is: {}</h2>
+                  <h2>Correct Answer is: {}</h2>
+                  <h2>Correct Answer is: {}</h2>
+                  <h2>Correct Answer is: {}</h2>
+                  <h2>Correct Answer is: {}</h2>
+                  '''.format(eval(question1), eval(question2), eval(question3), eval(question4), eval(question5))
+
+    # otherwise handle the GET request
+    return '''
+           <form method="POST">
+               <div><label>{}: <input type="text" name="answer1"></label></div>
+               <div><label>{}: <input type="text" name="answer2"></label></div>
+               <div><label>{}: <input type="text" name="answer3"></label></div>
+               <div><label>{}: <input type="text" name="answer4"></label></div>
+               <div><label>{}: <input type="text" name="answer5"></label></div>
+               <input type="submit" value="Submit">
+           </form>'''.format(question1, question2, question3, question4, question5)
 
 
 @app.route('/numberSense')
--- frontend/src/NumberSense/NumberSense.js
+++ frontend/src/NumberSense/NumberSense.js
@@ -4,13 +4,27 @@ import { readString } from 'papaparse';
 import $ from 'jquery';
 import './NumberSense.css';
 import axios from 'axios';
-import "./NumberSenseQuestions.csv" as Questions;
 
 
 function NumberSense(){
-  return() {
-    return <div>Data</div>
-  }
-}
+    const [question, setQuestion] = useState('Question Error');
 
+  useEffect(() => {
+    fetch('/numberSense').then(res => res.json()).then(data => {
+      setQuestion(data.result);
+    });
+    }, []);
+
+    return(
+        <html>
+            <div className="Test">
+                {question}
+              <form onSubmit={fetchData} method = "post">
+                 Answer:<input type ="text" name="ans" />
+                 <input type="submit" value = "submit" />
+              </form>
+            </div>
+        </html>
+    )
+}
 export default NumberSense;
