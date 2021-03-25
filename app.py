f6787fcc4fdce98631f962ff744863696267dd98
Kelsey McCutcheon <kelsey.mccutcheon@mymail.champlain.edu>
2021-03-24 23:26:25 -0400

    file placement changes

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_cors import CORS
+from flask import request
 import pandas as pd
 
 app = Flask(__name__)
@@ -8,9 +9,29 @@ CORS(app)
 
 def number_sense_test():
     data = pd.read_csv("NumberSenseQuestions.csv")
-    return data.QuestionD1[1]
+    ques_and_ans = []
+    for question in data.QuestionD1:
+        ques_dict = {
+            "question": question,
+            "answer": answer
+        }
+        ques_and_ans.append(ques_dict)
+    return ques_and_ans
+
+
+def answer(question):
+    correct = eval(question)
+    user_answer = request.args.get('user_answer')
+    if user_answer == correct:
+        return "Correct!"
+    else:
+        return "Incorrect!"
 
 
 @app.route('/numberSense')
 def number_sense():
     return {'result': number_sense_test()}
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- frontend/src/NumberSense/NumberSense.js
+++ frontend/src/NumberSense/NumberSense.js
@@ -2,10 +2,14 @@ import React from 'react';
 import {useState, useEffect} from 'react';
 import $ from 'jquery';
 import './NumberSense.css';
+import axios from 'axios';
+import '.././'
 
 
 function NumberSense(){
+    const [count, setCount] = useState(0);
     const [question, setQuestion] = useState('Question Error');
+    const [user, setUser] = useState('NULL')
 
   useEffect(() => {
     fetch('/numberSense').then(res => res.json()).then(data => {
@@ -13,12 +17,23 @@ function NumberSense(){
     });
     }, []);
 
+    useEffect(() => {
+    fetch('/check').then(res => res.json()).then(data => {
+      setUser(data.result);
+    });
+    }, []);
+
     return(
-        <div className="Test">
-            <div>
+        <html>
+            <div className="Test">
                 {question}
+              <form action="/check" method = "post">
+                 Answer:<input type ="text" name="ans" />
+                 <input onSubmit={() => setCount(count+1)} type="submit" value = "submit" />
+              </form>
+                {user}
             </div>
-        </div>
+        </html>
     )
 }
 
