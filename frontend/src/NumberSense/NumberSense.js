797b574bcde99927d9e3aadb689ec80b2d2b2aef
Kelsey McCutcheon <kelsey.mccutcheon@mymail.champlain.edu>
2021-03-25 01:24:56 -0400

    put files back

--- frontend/src/NumberSense/NumberSense.js
+++ frontend/src/NumberSense/NumberSense.js
@@ -1,40 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {useState, useEffect} from 'react';
+import { readString } from 'papaparse';
 import $ from 'jquery';
 import './NumberSense.css';
 import axios from 'axios';
-import '.././'
+import "./NumberSenseQuestions.csv" as Questions;
 
 
 function NumberSense(){
-    const [count, setCount] = useState(0);
-    const [question, setQuestion] = useState('Question Error');
-    const [user, setUser] = useState('NULL')
-
-  useEffect(() => {
-    fetch('/numberSense').then(res => res.json()).then(data => {
-      setQuestion(data.result);
-    });
-    }, []);
-
-    useEffect(() => {
-    fetch('/check').then(res => res.json()).then(data => {
-      setUser(data.result);
-    });
-    }, []);
-
-    return(
-        <html>
-            <div className="Test">
-                {question}
-              <form action="/check" method = "post">
-                 Answer:<input type ="text" name="ans" />
-                 <input onSubmit={() => setCount(count+1)} type="submit" value = "submit" />
-              </form>
-                {user}
-            </div>
-        </html>
-    )
+  return() {
+    return <div>Data</div>
+  }
 }
 
 export default NumberSense;
