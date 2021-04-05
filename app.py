e7d67e4b849a37e0a202eb6d139c58dbef348323
Kelsey McCutcheon <kelsey.mccutcheon@mymail.champlain.edu>
2021-04-05 10:50:33 -0400

    added dict which saves question info to be accessed throughout python functions

--- app.py
+++ app.py
@@ -10,6 +10,12 @@ CORS(app)
 
 data = pd.read_csv("NumberSenseQuestions.csv")
 
+info = {
+    "number": "num 1",
+    "question": " "
+}
+
+
 # iterate through csv questions
 # for now just implements random
 # num generator in all questions
@@ -17,8 +23,6 @@ def iterate_questions():
     ques_list = []
     for x in data.QuestionFormat:
         ques_list.append(random_num(x))
-
-    print(ques_list)
     return ques_list
 
 
@@ -57,21 +61,27 @@ def questions():
 # get first question separate since we always start
 # at topic 1, medium level
 # use index 1 of D1 for now
-@app.route('/question1')
-def question1():
-    question1 = data.QuestionD1[1]
-    return {'result': question1}
+@app.route('/question')
+def question():
+    ques = data.QuestionFormat[1]
+    ques_num = data.QuestionID[1]
+    user_ques = random_num(ques)
+    info["question"] = user_ques
+    info["number"] = ques_num
+    return info
 
 
 # request answer of previous question,
-# evaulate in sep function, return next question
+# evaluate in sep function, return next question
 @app.route('/nextQuestion', methods=['GET', 'POST'])
 def nextQuestion():
     user_answer = request.form.get("user_answer")
+    user_ques = info["question"]
+    print(info)
     if request.method == 'POST':
-        return evaluate(user_answer)
+        return evaluate(user_answer, user_ques)
     elif request.method == "GET":
-        return evaluate(user_answer)
+        return evaluate(user_answer, user_ques)
     else:
         return "Request Error"
 
@@ -81,8 +91,8 @@ def test_adapt_alg(user_answer):
     pass
 
 
-def evaluate(user_answer):
-    correct = eval(data.QuestionD1[1]) # will have to be changed when change to random num generator
+def evaluate(user_answer, user_ques):
+    correct = eval(str(user_ques))
     if str(correct) == user_answer:
         return {'result': 'True'}
     else:
--- frontend/src/NumberSense/NumberSense.js
+++ frontend/src/NumberSense/NumberSense.js
@@ -5,13 +5,22 @@ import './NumberSense.css';
 
 function NumberSense(){
     const [quesNum, setQuesNum] = useState(0);
+    const [count, setCount] = useState(0);
 
-    const [question1, setQuestion1] = useState("Question1 Error");
+    const [num, setNum] = useState('Question Number Error')
     useEffect(() => {
-        fetch('/question1').then(res => res.json()).then(data => {
-          setQuestion1(data.result);
+        fetch('/question').then(res => res.json()).then(data => {
+          setNum(data.number);
         });
     }, []);
+
+    const [ques, setQues] = useState("Question1 Error");
+    useEffect(() => {
+        fetch('/question').then(res => res.json()).then(data => {
+          setQues(data.question);
+        });
+    }, []);
+
 
     const [nextQuestion, setNextQuestion] = useState('Next Question Error');
     useEffect(() => {
@@ -33,35 +42,16 @@ function NumberSense(){
         });
     }, []);
 
-    const [answer1, setAnswer1] = useState('No Answer');
-    const [answer2, setAnswer2] = useState('No Answer');
-    const [answer3, setAnswer3] = useState('No Answer');
-    const [answer4, setAnswer4] = useState('No Answer');
-    const [answer5, setAnswer5] = useState('No Answer');
-    const [answer6, setAnswer6] = useState('No Answer');
-
-    useEffect(() => {
-        fetch('/answers').then(res => res.json()).then(data => {
-      setAnswer1(data.question1);
-      setAnswer2(data.question2);
-      setAnswer3(data.question3);
-      setAnswer4(data.question4);
-      setAnswer5(data.question5);
-    });
-    }, []);
-
+    var user_ques = ques
 
     return(
         <html>
             <div className="Test">
                 <p>
-                    {question}
-                    {question1}
                     <form action="/nextQuestion" method="POST">
-                        {question1}: <input type="text" name="user_answer" ></input><br></br>
-                        <input type="submit" value="Submit"></input>
+                        {num}  {ques}: <input type="text" name="user_answer" ></input><br></br>
+                        <input type="submit" value="Submit" name="count"></input>
                     </form>
-                    {nextQuestion}
                 </p>
             </div>
         </html>
