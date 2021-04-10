a84fca2d57d0a815eb16ea51c499ae92dc88f0a6
Kelsey McCutcheon <kelsey.mccutcheon@mymail.champlain.edu>
2021-04-09 20:18:18 -0400

    pls deploy

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import json
 from random import randint
 from TestAdaptationAlgorithm import adaptAlgo
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='', static_folder='frontend/build')
 
 data = pd.read_csv("NumberSenseQuestions.csv")
 
