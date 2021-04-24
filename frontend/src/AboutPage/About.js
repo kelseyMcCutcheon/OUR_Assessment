9c7d7ec2026ab3b050de0283e5e978117e478249
AdamHasTheBeans <adam.currier@mymail.champlain.edu>
2021-04-24 17:32:15 -0400

    Forgot to add about page info

--- frontend/src/AboutPage/About.js
+++ frontend/src/AboutPage/About.js
@@ -12,7 +12,11 @@ function AboutPage() {
 
             <h1 className='AboutHeader'>About Our Assessment</h1>
             <p className='AboutInfo' id="AboutText">
-                Our Assessment was made by Adam, Kelsey, Sawyer.
+                &emsp;Our Assessment was created by Sawyer Eden Cawley-Edwards, Adam Currier, and Kelsey McCutcheon.
+                The purpose of this capstone project is to create a competency-based mathematics placement test, 
+                the focus of which is to provide students with an unbiased assessment of their actual mathematics skills. 
+                With This assessment we hope to determine whether or not a student is considered ‘competent’ in a certain mathematical level. 
+                As well as give students useful and personalized information about what specific concepts they need to focus their efforts on.
             </p>
         </div>
     );
